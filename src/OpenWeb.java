7965cf5e686de7371df4ee81415c16d555dadbce
zawawimanja <zawawimanja@gmail.com>
2022-03-13 11:18:24 +0800

    OpenWeb

---
+++ src/OpenWeb.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenWeb {
+
+    public static void main(String[] args) {
+
+
+        //System.out.println("hello");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\zawawi.manja\\Downloads\\chromedriver.exe" );
+        WebDriver driver=new ChromeDriver();
+        driver.get(("http://demowebshop.tricentis.com/"));
+        driver.quit();
+
+
+
+    }
+}
